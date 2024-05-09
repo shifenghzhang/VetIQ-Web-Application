ed3e6b796157c20edb1fa743ff238e8b17897c17
HoangLe-s3920128 <s3920128@student.rmit.edu.au>
2024-05-09 14:55:17 +1000

    Update hero-section.tsx

--- src/app/_components/hero-section.tsx
+++ src/app/_components/hero-section.tsx
@@ -3,22 +3,19 @@ import image from "public/images/iStock-1224227548.jpg";
 
 const HeroSection = () => {
     return (
-        <section className="text-gray-600 body-font">
-  <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
-    <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-      <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">VET IQ 
-    
-      </h1>
-      <p className="mb-8 leading-relaxed">Introductory Text.</p>
-      <div className="flex justify-center">
-        <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Demo</button>
+      <section className="text-gray-600 body-font">
+      <div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col">
+      <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src="public\images\iStock-1224227548.jpg"></img>
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">VET IQ</h1>
+          <p className="mb-8 leading-relaxed">Introductory Text.</p>
+          <div className="flex justify-center">
+            <button className="inline-flex text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+            <button className="ml-4 inline-flex text-gray-700 bg-gray-100 border-0 py-2 px-6 focus:outline-none hover:bg-gray-200 rounded text-lg">Button</button>
+          </div>
+        </div>
       </div>
-    </div>
-    <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
-      
-    </div>
-  </div>
-</section>
+    </section>
     )
 }
 
