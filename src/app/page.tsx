3ca6619ac5ae6599766d5f89e80866448b17d992
HoangLe-s3920128 <s3920128@student.rmit.edu.au>
2024-05-16 14:30:07 +1000

    Revert "Merge branch 'HeroSection-&-Testimonial' into helpSection"
    This reverts commit 5af79675ace2a72e409c8c6e68cc37e5657e4dd3, reversing
    changes made to bef328977570b1b4cb8c70d4eab36c111fd78baa.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,27 +11,54 @@ const articles: Article[] = [
   {
     id: 1,
     title: 'Leveraging Intelligent Data Analysis for Veterinary Excellence with VetIQ',
-    summary: 'VetIQ by Curious Cat transforms veterinary practice management with smart data analysis, providing actionable insights for growth and operational excellence. Make informed decisions to improve clinical services and client satisfaction.',
+    summary: [
+    'VetIQ, powered by Curious Cat, revolutionizes veterinary practice management\n',
+    'Transforms complex data into intelligent, actionable insights\n',
+    'Goes beyond traditional data analysis, focusing on delivering precise information\n',
+    'Fosters tangible growth and operational excellence\n',
+    'Embrace VetIQ data-driven solutions to make informed decisions\n',
+    'Enhances both clinical services and client satisfaction\n',
+    ],
   },
   {
     id: 2,
     title: 'Advanced Analytics Transcending Traditional Approaches',
-    summary: 'VetIQ streamlines data analysis in veterinary practices, replacing manual efforts with a cutting-edge analytics engine. Veterinarians gain intuitive insights, enabling a shift from reactive to proactive strategies for growth and efficiency.',
+    summary: [
+    'Traditional data analysis in veterinary practices often involves manual sifting through extensive reports\n',
+    'Struggles to derive meaningful insights\n',
+    'VetIQ transforms this process with its state-of-the-art analytics engine\n',
+    'Provides veterinarians with intuitive and actionable insights\n',
+    'Enables a proactive approach focused on strategic growth and operational efficiency\n',
+    ],
   },
     {
       id: 3,
       title: 'Client Engagement',
-      summary: 'VetIQ enhances client understanding by offering insights into loyalty, acquisition, and retention. Identify opportunities for better engagement and satisfaction.',
+      summary: [
+      'VetIQ helps you understand your clients better\n',
+      'Provides insights into client loyalty, client acquisition, and client retention\n',
+      'Helps you identify opportunities to improve client engagement and satisfaction\n',
+      ],
     },
     {
       id: 4,
       title: 'Empowering Veterinarians with Data-Driven Decisions',
-      summary: 'VetIQ empowers veterinarians with actionable insights for informed business decisions. Its advanced analytics identify opportunities and areas for improvement, from drug inventories to pricing and patient care protocols.',
+      summary: [
+      'VetIQ is about empowering veterinarians with the knowledge they need to make enlightened business decisions\n',
+      'The platform\'s sophisticated analytics cut through the noise\n',
+      'Highlights opportunities and pinpoints areas in need of attention\n',
+      'Provides insights for optimizing drug inventories, adjusting pricing strategies, or enhancing patient care protocols\n',
+      'Enables well-informed decision making\n',
+      ],
     },
     {
       id: 5,
       title: 'Business Performance',
-      summary: 'VetIQ optimizes business performance with insights on finances, operations, and growth. Identify opportunities for improvement and achieve your goals.',
+      summary: [
+      'VetIQ helps you optimize your business performance\n',
+      'Provides insights into your financial performance, operational efficiency, and strategic growth\n',
+      'Helps you identify opportunities to improve your business and achieve your goals\n',
+      ],
     }
 ];
 export default function Home() {
