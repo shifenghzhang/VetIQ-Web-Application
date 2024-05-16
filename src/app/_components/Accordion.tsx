507dfb01c49db13ea4e1cd5b3a6d6382068c7e18
HoangLe-s3920128 <s3920128@student.rmit.edu.au>
2024-05-16 14:36:19 +1000

    Reapply "Reapply "helpSection commit 2 13/05""
    This reverts commit 724ad654eecae695493a10135fea46fb20a70a30.

---
+++ src/app/_components/Accordion.tsx
@@ -0,0 +1,58 @@
+import React, { useState } from "react";
+
+interface AccordionProps {
+  question: string;
+  answer: React.ReactNode;
+}
+
+const Accordion: React.FC<AccordionProps> = ({ question, answer }) => {
+  const [accordionOpen, setAccordionOpen] = useState(false);
+
+  return (
+    <div className="py-2">
+      <button
+        onClick={() => setAccordionOpen(!accordionOpen)}
+        className="flex justify-between w-full"
+      >
+        <span>{question}</span>
+        {/*accordionOpen? <span>-</span> : <span>+</span>*/}
+        <svg
+          className="fill-indigo-500 shrink-0 ml-8"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center transition duration-200 ease-out ${
+              accordionOpen && "!rotate-180"
+            }`}
+          />
+          <rect
+            y="7"
+            width="16"
+            height="2"
+            rx="1"
+            className={`transform origin-center rotate-90 transition duration-200 ease-out ${
+              accordionOpen && "!rotate-180"
+            }`}
+          />
+        </svg>
+      </button>
+      <div
+        className={`grid overflow-hidden transition-all duration-300 ease-in-out text-slate-600 text-sm ${
+          accordionOpen
+           ? "grid-rows-[1fr] opacity-100"
+            : "grid-rows-[0fr] opacity-0"
+        }`}
+      >
+        <div className="overflow-hidden">{answer}</div>
+      </div>
+    </div>
+  );
+};
+
+export default Accordion;
