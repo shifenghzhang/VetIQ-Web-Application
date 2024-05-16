0513f5c072c15dd0bbd9709d511a683693b06d3c
HoangLe-s3920128 <s3920128@student.rmit.edu.au>
2024-05-16 14:26:47 +1000

    Revert "helpSection commit 2 13/05"
    This reverts commit bef328977570b1b4cb8c70d4eab36c111fd78baa.

--- src/app/help/page.tsx
+++ src/app/help/page.tsx
@@ -1,19 +1,10 @@
-"use client";
-
-import React from 'react';
-import FAQ from '../_components/FAQ'; 
-import ContactUs from '../_components/contactUs';
-
+import React from 'react'
+import ContactUs from '../_components/contactUs'
 
 function page() {
   return (
     <main>
-      <div>
-        <div>
-        <FAQ />
-         </div>
-      </div>
-      <ContactUs />   
+      <ContactUs />
     </main>
   )
 }
