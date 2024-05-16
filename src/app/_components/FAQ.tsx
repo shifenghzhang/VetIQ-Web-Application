097e359dc7e6b53c8a4f7d923c6dc42ced2119c1
HoangLe-s3920128 <s3920128@student.rmit.edu.au>
2024-05-16 15:02:43 +1000

    commit 3

--- src/app/_components/FAQ.tsx
+++ src/app/_components/FAQ.tsx
@@ -5,7 +5,8 @@ const FAQ = () => {
     return (
         <div>
             <h1 className="flex flex-col text-center w-full sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">FAQ</h1>
-        <div className="p-4 bg-gray-200 rounded-lg">
+        <div className="flex justify-center items-center h-full">
+            <div className="p-4 bg-gray-200 rounded-lg w-3/4">
             <Accordion 
                 question="Question 1" 
                 answer="answer 1" />
@@ -15,6 +16,7 @@ const FAQ = () => {
             <Accordion 
                 question="Question 3" 
                 answer="answer 3" />
+            </div>
         </div>
         </div>
     );
