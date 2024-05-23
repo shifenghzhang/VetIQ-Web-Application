6905e9f286cdac5b9d6a23872e03e42fd1aca2e2
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-05-23 17:35:35 +1000

    mongo db - get users, add and delete a user methods

--- python-service/routes/users_routes.py
+++ python-service/routes/users_routes.py
@@ -1,5 +1,6 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from db_config import get_db_connection
+from mongodb import users_collection
 
 user_bp = Blueprint('user_bp', __name__)
 
@@ -14,5 +15,30 @@ def get_users():
         result = [dict(zip(columns, row)) for row in rows]
         conn.close()
         return jsonify(result)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@user_bp.route('/mongo_users', methods=['GET'])
+def get_mongo_users():
+    try:
+        users = list(users_collection.find({}, {'_id': 0}))
+        return jsonify(users)
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@user_bp.route('/add_mongo_user', methods=['POST'])
+def add_mongo_users():
+    try:
+        data = request.json
+        users_collection.insert_one(data)
+        return jsonify({'message': 'user added successfully'})
+    except Exception as e:
+        return jsonify({'error': str(e)})
+    
+@user_bp.route('/delete_mongo_user/<user_id>', methods=['DELETE'])
+def delete_mongo_user(user_id):
+    try:
+        users_collection.delete_one({"user_id": user_id})
+        return jsonify({"message": "User deleted successfully."})
     except Exception as e:
         return jsonify({'error': str(e)})
