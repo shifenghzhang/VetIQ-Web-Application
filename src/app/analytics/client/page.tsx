714bf3ba27f78268ee3ffaea7ddf35281de428f2
Shrey-Singhal <s3916734@student.rmit.edu.au>
2024-06-14 12:02:10 +1000

    analytics client modal

--- src/app/analytics/client/page.tsx
+++ src/app/analytics/client/page.tsx
@@ -1,9 +1,37 @@
+"use client"
 import React from 'react'
+import AnalyticsModal from '~/app/_components/analyticsModal';
+import { useAuth } from '~/app/_contexts/authProvider';
+import { useEffect, useState } from 'react';
 
-function page() {
+function Page() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+
+  const handleModalClose = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleModalSubmit = (answers: string) => {
+    console.log("Submitting survey data:", answers);    
+  };
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsModalOpen(true);
+    }, 1000); // 5 seconds delay
+
+    return () => clearTimeout(timer); // Cleanup timer on component unmount
+  }, []);
   return (
-    <div>Client</div>
+    <div>
+      <AnalyticsModal 
+        isOpen={isModalOpen}
+        onRequestClose={handleModalClose}
+        onSubmit={(answers) => handleModalSubmit(answers)}
+      />
+    </div>
   )
 }
 
-export default page;
+export default Page;
